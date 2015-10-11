700c714f8419805963a7674d4acd57349fe71bee
Jordan Tang <jordantang0229@gmail.com>
2015-10-11 00:32:51 -0700

    Lab 4 progress up to curry2

--- Lab4.rkt
+++ Lab4.rkt
@@ -7,6 +7,7 @@
  (lambda (x) (* x 2)))
 
 ;Takes number a, returns function that takes number b, adds a + b
+;(number -> (number -> number))
 (define curried-add  
   (lambda (a)
     (lambda (b) (+ a b))))
@@ -14,3 +15,9 @@
 (test ((curried-add 5) 3) 8)
 (test ((curried-add 0) -5) -5)
 
+;Takes a function of two arguments, produces a function that takes one argument,
+;Produces a function that takes one argument and produces the result of calling input function on two given arguments
+;(a b -> c) -> (a -> (b -> c))
+(define curry2
+  (lambda (a b)
+    (lambda (c) )))
