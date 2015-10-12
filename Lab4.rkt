b1c7144a277a4a367dec58d7acbb038a0beeefb9
Jordan Tang <jordantang0229@gmail.com>
2015-10-11 21:55:10 -0700

    Finished Lab 4

--- Lab4.rkt
+++ Lab4.rkt
@@ -8,7 +8,7 @@
 
 ;Takes number a, returns function that takes number b, adds a + b
 ;(number -> (number -> number))
-(define (curried-add [a : number]) : number
+(define (curried-add [a : number])
   (lambda (b) (+ a b)))
 
 (test ((curried-add 5) 3) 8)
@@ -17,8 +17,20 @@
 ;Takes a function of two arguments, produces a function that takes one argument,
 ;Produces a function that takes one argument and produces the result of calling the input function on two given arguments
 ;(a b -> c) -> (a -> (b -> c))
-(define (curry2 func) : number
+(define (curry2 func)
   (lambda (a)
     (lambda (b) (func a b))))
 
-;(test (((curry2 3 4) 5) ))
+(test (((curry2 (lambda (a b) (+ a b))) 2) 4) 6)
+(test (((curry2 (lambda (a b) (* (- a b) b))) 5) 3) 6)
+
+;Takes function of three arguments, produces a function that takes one argument,
+;Produces a function that takes one argument and produces another function that produces
+;the result of calling the input function on all three arguments
+(define (curry3 func)
+  (lambda (a)
+    (lambda (b)
+      (lambda (c) (func a b c)))))
+
+(test ((((curry3 (lambda (a b c) (+ (+ a b) c))) 1) 2) 3) 6)
+(test ((((curry3 (lambda (a b c) (* a (+ a (- c b))))) 2) 5) 6) 6)
